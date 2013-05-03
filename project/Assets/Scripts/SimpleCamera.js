a08fa1ecf8e63c139b01c8ae6cb64ba4cfc4b012
yikeda <yikeda.dev@gmail.com>
2013-05-04 01:25:16 +0900

    fixed

---
+++ project/Assets/Scripts/SimpleCamera.js
@@ -0,0 +1,47 @@
+#pragma strict
+
+var cubeObject : GameObject;
+
+private var webcamTexture : WebCamTexture;
+private var texture : Texture2D;
+private var data : Color32[];
+
+private var initDone : boolean = false;
+
+function Start()
+{
+
+	webcamTexture = WebCamTexture(8, 8, 12);
+    webcamTexture.Play();
+
+	while(1)
+    {
+    	if (webcamTexture.didUpdateThisFrame)
+    	{
+    		data = new Color32[webcamTexture.width * webcamTexture.height];
+
+    		texture = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.RGB565, false);
+
+    		cubeObject.renderer.material.SetTexture("_MainTex", texture);
+
+    		initDone = true;
+
+    		break;
+    	}
+    	else
+    	{
+    		yield;
+    	}
+    }
+}
+
+function Update()
+{
+	if (initDone && webcamTexture.didUpdateThisFrame)
+	{
+		webcamTexture.GetPixels32(data);
+
+		texture.SetPixels32(data);
+		texture.Apply();
+	}
+}
---
+++ project/Assets/Scripts/camera.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+    const int GridSize = 16;
+
+	WebCamDevice[] devices;
+	WebCamTexture webcamTexture;
+	Texture2D smallTexture;
+	Color32[] basePixels;
+	Color32[] displayPixels;
+	int[] grayscalePixels;
+	int[] grayscalePixelsPrev;
+	int[] grayscalePixelsCount;
+    bool isInitialized;
+
+	void Start () {
+        isInitialized = false;
+		devices  = WebCamTexture.devices;
+		if (devices.Length > 1) {
+			webcamTexture  = new WebCamTexture(devices[1].name, 128, 128, 60);
+			webcamTexture.Play();
+		} else {
+		}
+	}
+
+    void InitializeTexture ()
+    {
+        if (webcamTexture != null &&  webcamTexture.didUpdateThisFrame) {
+            GameObject plane1 = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            GameObject plane2 = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            plane1.transform.position = new Vector3(0, 5.0f, 0);
+            plane1.transform.Rotate(-90,0,0);
+            plane2.transform.position = new Vector3(0, -5.0f, 0);
+            plane2.transform.Rotate(-90,0,0);
+
+            smallTexture = new Texture2D(GridSize,GridSize);
+            basePixels = new Color32[webcamTexture.width * webcamTexture.height];
+            displayPixels = new Color32[GridSize * GridSize];
+            grayscalePixels = new int[GridSize * GridSize];
+            grayscalePixelsPrev = new int[GridSize * GridSize];
+            grayscalePixelsCount = new int[GridSize * GridSize];
+
+            plane1.renderer.material.mainTexture = smallTexture;
+            plane1.renderer.material.mainTexture.filterMode = FilterMode.Point;
+            plane1.renderer.material.shader = Shader.Find("Unlit/Texture");
+
+            plane2.renderer.material.mainTexture = webcamTexture;
+            //plane2.renderer.material.mainTexture.filterMode = FilterMode.Point;
+            //plane2.renderer.material.shader = Shader.Find("Unlit/Texture");
+
+            Debug.Log(webcamTexture.width);
+            Debug.Log(webcamTexture.height);
+
+            isInitialized = true;
+        }
+    }
+
+	void Update () {
+        if (!isInitialized) {
+            InitializeTexture();
+            return;
+        }
+
+        if (!webcamTexture.didUpdateThisFrame)
+            return;
+
+        webcamTexture.GetPixels32(basePixels);
+		grayscalePixels.CopyTo(grayscalePixelsPrev, 0);
+
+		for (int x = 0; x < GridSize; x++) {
+			for (int y = 0; y < GridSize; y++) {
+				int index = x + GridSize * y;
+				grayscalePixels[index] = 0;
+				grayscalePixelsCount[index] = 0;
+			}
+		}
+
+		for (int x = 0; x < webcamTexture.width; x++) {
+			for (int y = 0; y < webcamTexture.height; y++) {
+				Color32 color = basePixels[x + webcamTexture.width * y];
+				int small_x = x / (webcamTexture.width / GridSize);
+				int small_y = y / (webcamTexture.height / GridSize);
+				int index = small_x + GridSize * small_y;
+				int gray = (int)(0.299 * (int)color.r + 0.587 * (int)color.g + 0.114 * (int)color.b);
+				grayscalePixels[index] += gray;
+				grayscalePixelsCount[index]++;
+			}
+		}
+
+		for (int x = 0; x < GridSize; x++) {
+			for (int y = 0; y < GridSize; y++) {
+				int index = x + GridSize * y;
+				byte gray = (byte)(grayscalePixels[index] / grayscalePixelsCount[index]);
+				displayPixels[index].r = gray;
+				displayPixels[index].g = gray;
+				displayPixels[index].b = gray;
+				displayPixels[index].a = (byte)1;
+			}
+		}
+
+		smallTexture.SetPixels32(displayPixels);
+		smallTexture.Apply();
+	}
+}
